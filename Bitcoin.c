354f4c397c8411cc9b07edd78c0a8f01127834d1
Kurush Dubash <dubash.kurush@gmail.com>
2014-06-09 16:44:40 -0700

    Added Serial1

--- Bitcoin.c
+++ Bitcoin.c
@@ -15,6 +15,7 @@
  void setup() {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  Serial1.begin(9600);
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
