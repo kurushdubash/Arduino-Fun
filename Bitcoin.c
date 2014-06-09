9aea68944bdc0771bdc8ba10607e2af2d2d2de6a
Kurush Dubash <dubash.kurush@gmail.com>
2014-06-09 16:20:11 -0700

    First Upload

---
+++ Bitcoin.c
@@ -0,0 +1,14 @@
+/*
+  Bitcoin Price Fetcher
+ 
+ This sketch connects to a coinbase (https://coinbase.com/api/v1/prices/spot_rate)
+ using a sparkcore. 
+ 
+
+ created 9 June 2014
+ by Kurush Dubash
+ by Hassaan Shakeel
+ 
+ */
+
+ char server[] = "https://coinbase.com/api/v1/prices/spot_rate";    
