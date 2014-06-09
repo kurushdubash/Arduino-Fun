fa36cd775169d525ca851e8d0eab477b2d960572
Kurush Dubash <dubash.kurush@gmail.com>
2014-06-09 16:29:51 -0700

    Testing the commit system as an example.

--- Bitcoin.c
+++ Bitcoin.c
@@ -12,3 +12,34 @@
  */
 
  char server[] = "https://coinbase.com/api/v1/prices/spot_rate";    
+ void setup() {
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  delay(1000);
+  Serial.println("Connecting...");
+  Serial.println("Connected");
+}
+
+void loop()
+{
+  // if there are incoming bytes available 
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while(true);
+  }
+}
